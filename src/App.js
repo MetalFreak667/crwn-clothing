46ae31537c4ee506147971554e9c86076ab5b946
Krešimir Delija <net4metal@gmail.com>
2019-08-07 22:25:05 +0200

    sign in form with custom form and button components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component.jsx'
+import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 const HatsPage = () =>
 (
@@ -21,6 +22,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop/' component={ShopPage}></Route>
+        <Route path='/signin/' component={SignInAndSignOut}></Route>
       </Switch>    
     </div>
   );
