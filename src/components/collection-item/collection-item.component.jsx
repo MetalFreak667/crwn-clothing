37862042e00ac86f3def41a822cfa31f0fbbb6fe
Krešimir Delija <net4metal@gmail.com>
2019-08-15 07:49:04 +0200

    added cart selectors

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import './collection-item.styles.scss'
+import CustaomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions.js';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;
+    return (
+        <div className='collection-item'>
+            <div
+            className='image'
+            style={{backgroundImage: `url(${imageUrl})`}}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{ name }</span>
+                <span className='price'>{ price }</span>
+            </div>        
+            <CustaomButton 
+            inverted
+            onClick={() => addItem(item)}
+            > Add to cart </CustaomButton>
+        </div>
+    );
+};
 
-    <div key={id} className='collection-item'>
-        <div
-        className='image'
-        style={{backgroundImage: `url(${imageUrl})`}}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{ name }</span>
-            <span className='price'>{ price }</span>
-        </div>        
-    </div>
+const mapDispatchToProps = dispatch =>({
+    addItem: item=> dispatch(addItem(item))
+})
 
-)
-
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
